8811c5bc29b91f0dabc9f9170254d48423a77e6a
John O'Sullivan <josullivan1@luc.edu>
2018-07-16 18:21:07 -0700

    Init

---
+++ lib/demo_api.ex
@@ -0,0 +1,18 @@
+defmodule DemoAPI do
+  @moduledoc """
+  Documentation for DemoAPI.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DemoAPI.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
